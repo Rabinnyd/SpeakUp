bd8394fc9a5deba989bbfafc9453eea123e7a232
polojerry <polojerry4@gmail.com>
2020-03-05 10:50:17 +0300

    Initial commit

---
+++ app/src/main/java/com/polotechnologies/speakup/uiHost/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.polotechnologies.speakup.uiHost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.polotechnologies.speakup.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
